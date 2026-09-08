fbf7844ca5d56ccd366b1e7c0a3ebbfe5f6e147a
Letder40 <adrian04rv@gmail.com>
2026-09-08 04:03:59 +0200

    bug fixes...

--- qtile/autostart.py
+++ qtile/autostart.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import subprocess
-import tomllib
 from libqtile import hook
 
 from user_config import config
@@ -8,13 +7,10 @@ from user_config import config
 
 @hook.subscribe.startup_once
 def autostart():
-    with open(Path.home() / ".config/qtile/config.toml", "rb") as f:
-        config = tomllib.load(f)
-
     if config["preferences"]["animations"]:
-        config_path = Path.home() / ".config" / "picom.conf"
-        subprocess.Popen(["picom", "-b", "--config", config_path.absolute().as_posix()]) 
+        config_path = Path.home() / ".config" / "picom" / "picom.conf"
+        subprocess.Popen(["picom", "-b", "--config", config_path.absolute().as_posix()])
 
     for entry in config["packages"]["init"]:
-        subprocess.run(["kill", "-9", entry["exec"].split()[0]]) 
-        subprocess.Popen(entry["exec"].split()) 
+        subprocess.run(["kill", "-9", entry["exec"].split()[0]])
+        subprocess.Popen(entry["exec"].split())
